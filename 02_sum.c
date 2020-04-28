a70df8b34ffcd1a6b62253a268ba141840fc7cc1
hanteruo <oxota@root.kali>
2020-04-28 12:54:26 +0700

    practica

---
+++ 02_sum.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+double n;
+double m;
+(void)scanf("%lf %lf", &n, &m);
+printf("%lf\n", n+m);
+return 0;
+}
---
+++ 03_pow.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+
+#include <math.h>
+
+int main()
+
+    {
+    double v,w;
+    (void)scanf("%lf", &v);
+    (void)scanf("%lf", &w);
+    printf("%lf\n", pow(v,w));
+    return 0; 
+     }
